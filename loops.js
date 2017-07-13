72da5794a78810123de3f87d8f386b4f85c705cc
Sharowynn Wilson <snwilson@csuchico.edu>
2017-07-13 15:46:46 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(array) {
+	for (var i = 0; i < 25; i++) {
+    if ( i===1) {
+      array.push ('I am 1 strange loop.')
+    } else {
+        array.push (`I am ${i} loops`)
+    };
+  }
+return(array)}
+
+function whileLoop(n) { i=50
+  while (i > 0){
+    console.log('i');
+    i--;
+  }
+return('done')}
