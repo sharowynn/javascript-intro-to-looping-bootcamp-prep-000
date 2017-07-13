626bb1eb7602b768d1c479f777da8161640fbb33
Sharowynn Wilson <snwilson@csuchico.edu>
2017-07-13 17:43:08 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,16 +1,26 @@
 function forLoop(array) {
 	for (var i = 0; i < 25; i++) {
     if ( i===1) {
-      array.push ('I am 1 strange loop.')
-    } else {
-        array.push (`I am ${i} loops`)
+        array.push ('I am 1 strange loop.')
+   } else {
+       array.push (`I am ${i} loops`)
     };
   }
-return(array)}
+return(array)};
 
-function whileLoop(n) { i=50
-  while (i > 0){
-    console.log('i');
-    i--;
-  }
-return('done')}
+function whileLoop(n) {
+
+ while (n > 0) {
+  console.log(--n)
+}
+return'done'
+}
+
+function doWhileLoop(array) {
+	function maybeTrue() {
+  return Math.random() >= 0.5
+}
+	do {array = array.slice
+	} while (array.length > -0 && maybeTrue());
+	return (array)
+}
